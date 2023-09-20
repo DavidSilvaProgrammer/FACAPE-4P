b112bf075f7ab7485f40f4c8608a304c8e94e4cd
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2023-09-20 05:57:42 -0300

    Update CriarClasseObjeto.java

--- POO/CriarClasseObjeto.java
+++ POO/CriarClasseObjeto.java
@@ -11,3 +11,28 @@ public class Pessoa {
         System.out.println("Idade: " + pessoa1.idade);
     }
 }
+
+************************
+//Como usar método:
+
+public class MinhaClasse {
+    // Método simples que não recebe parâmetros e não retorna valor (void)
+    public void saudacao() {
+        System.out.println("Olá, mundo!");
+    }
+
+    // Método que recebe dois números inteiros como parâmetros e retorna a soma deles
+    public int somar(int num1, int num2) {
+        int resultado = num1 + num2;
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        MinhaClasse objeto = new MinhaClasse();
+
+        objeto.saudacao(); // Chamando o método saudacao
+        int soma = objeto.somar(5, 3); // Chamando o método somar com parâmetros e armazenando o resultado em uma variável
+        System.out.println("A soma é: " + soma);
+    }
+}
+
