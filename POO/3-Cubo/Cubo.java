7c91af9105b8913426b68b3fc43a4a4ab32a72d9
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2023-10-09 18:32:47 -0300

    Create Cubo.java

---
+++ POO/3-Cubo/Cubo.java
@@ -0,0 +1,15 @@
+
+class Cubo{
+    
+    public double arresta;
+    
+    public Cubo (double arresta){
+        this.arresta=arresta;
+    }
+    
+    public double calculaVolume(){
+        return Math.pow(arresta,3);
+    }
+    
+    
+}
