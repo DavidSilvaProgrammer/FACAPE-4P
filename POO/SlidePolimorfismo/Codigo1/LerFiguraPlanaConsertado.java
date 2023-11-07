070f50287dac3bff462ed1c22a346ed56da83c30
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2023-11-07 20:34:15 -0300

    Create LerFiguraPlanaConsertado.java

---
+++ POO/SlidePolimorfismo/Codigo1/LerFiguraPlanaConsertado.java
@@ -0,0 +1,50 @@
+class LerFiguraPlana{
+
+public static char lerTipo() throws Exception {
+    char tipo;
+    do {
+        System.out.print("\nInforme 'R' para Retangulo, 'C' para Circulo ou 'F' para Fim-> ");
+        String input = JUtil.readString();
+        if (input.length() > 0) {
+            tipo = input.charAt(0);
+        } else {
+            tipo = ' '; // Valor padrão se não for inserido nada
+        }
+    } while ((tipo != 'R') && (tipo != 'C') && (tipo != 'F'));
+    return tipo;
+}
+
+    
+    public static void main(String[] args) throws Exception{ 
+        FiguraPlana fp = new FiguraPlana () ;
+        char tipo; // Retangulo ou Circulo
+        System.out.println ("Dados da Figura Plana");
+        tipo = lerTipo ();
+        while (tipo != 'F'){
+            switch ( tipo){
+                case 'R':{
+                    fp = new Retangulo(0,0);
+                    System.out.print ("\nInforme a base -> ");
+                    ((Retangulo) fp).setLadoa (JUtil.readFloat());
+                    System.out.print ("\nInforme a largura -> ");
+                    ((Retangulo) fp).setLadob (JUtil.readFloat());
+                        break;
+                }
+                case 'C':{
+                    fp = new Circulo(0);
+                    System.out.print ("\nInforme o raio -> ");
+                    ((Circulo) fp).setRaio (JUtil.readFloat ());
+                    break;
+                }
+            } // switch
+            
+            fp.mostrarDados (); // polimorfismo aqui
+            if ( fp instanceof Circulo ){ 
+                System.out.println ("\n Area em PI do "+"circulo com comprimento 20 PI "+((Circulo)fp).area (20));
+            
+            } //switch
+        tipo = lerTipo ();
+        } // while
+    } // main
+  }// class
+                
