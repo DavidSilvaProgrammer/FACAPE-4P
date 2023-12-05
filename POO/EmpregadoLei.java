558f4c64e0591e40417accbfb96117e144c61c86
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2023-12-05 20:17:16 -0300

    Create EmpregadoLei.java

---
+++ POO/EmpregadoLei.java
@@ -0,0 +1,26 @@
+/*Crie uma classe concreta EmpregadoLei que herda as características de Empregado. A inicialização 
+é conforme a classe Empregado. O método que altera o salarioBase deve ser modificado. 
+Conforme a lei, o novo salário
+informado (passado como parâmetro) só pode ser maior que o atual. Caso
+contrário deve ser dada uma mensagem apropriada.*/
+
+public class EmpregadoLei extends Empregado
+{ 
+    
+    public EmpregadoLei (double matricula, String nome, double salarioBase){
+        
+        super(matricula, nome, salarioBase);
+    }
+        @Override
+        public void setSalarioBase(double salarioBase){
+            if (salarioBase > this.salarioBase){
+            this.salarioBase= salarioBase;
+        }else{
+            System.out.println("Valor so pode ser maior que o atual, conforme a Lei\n");
+        }
+        
+        
+    }
+        
+
+}
