9d50af65e5e1ecfd17f337d71a15ed8ef35dad6d
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2023-12-12 10:16:26 -0300

    Create CalculaRaid5VariavelMenor.py

---
+++ Arquitetura/CalculaRaid5VariavelMenor.py
@@ -0,0 +1,21 @@
+def calcular_raid5():
+    # Solicita ao usuário a quantidade de discos
+    quantidade_discos = int(input("Digite a quantidade de discos: "))
+    
+    # Inicializa a variável 'menor' com um valor muito alto
+    menor = float('inf')
+    
+    # Solicita ao usuário o valor de cada disco
+    for i in range(quantidade_discos):
+        disco = int(input(f"Digite o valor do disco {i+1}: "))
+        if disco < menor:
+            menor = disco
+    
+    # Calcula a capacidade de armazenamento
+    capacidade_armazenamento = (quantidade_discos - 1) * menor
+    
+    # Imprime a capacidade de armazenamento
+    print(f"A capacidade de armazenamento é: {capacidade_armazenamento} GB")
+
+# Chama a função
+calcular_raid5()
