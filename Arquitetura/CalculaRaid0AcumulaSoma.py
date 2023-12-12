8f9c02d6d01a0d33bf89cff810ee56c57e8b7017
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2023-12-12 09:38:34 -0300

    Create CalculaRaid0AcumulaSoma.py

---
+++ Arquitetura/CalculaRaid0AcumulaSoma.py
@@ -0,0 +1,22 @@
+def calculate_raid0_storage():
+    # Solicita ao usuário o número de discos
+    num_disks = int(input("Digite o número de discos: "))
+
+    # Inicializa a soma das capacidades
+    sum_capacity = 0
+
+    # Solicita ao usuário a capacidade de cada disco
+    for i in range(num_disks):
+        capacity = float(input(f"Digite a capacidade do disco {i+1} em TB: "))
+        # Adiciona a capacidade ao total
+        sum_capacity += capacity
+
+    # Calcula a capacidade total de armazenamento em RAID 0
+    total_capacity = sum_capacity / num_disks
+
+    # Imprime a capacidade total de armazenamento em RAID 0
+    print(f"A capacidade total de armazenamento em RAID 0 é {total_capacity}TB.")
+
+# Chama a função
+calculate_raid0_storage()
+
